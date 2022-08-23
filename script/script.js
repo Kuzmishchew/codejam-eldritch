fef71430bb441ed012873b84b01aba03f44d6e65
Kuzmishchew <kuzmishchew@gmail.com>
2022-08-23 10:39:10 +0500

    feat: change link in modules assets

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
 import ancientsData from '../data/ancients.js';
+import greenCardsData from '../data/mythicCards/green/index.js';
+import brownCardsData from '../data/mythicCards/brown/index.js';
+import blueCardsData from '../data/mythicCards/blue/index.js';
+
 
 const DIFFICLUTIES_INPUTS = document.getElementsByName('difficulty-level');
+const MIX_BUTTON = document.querySelector('.mix-button');
 
 let currentAncient = {};
 let currentDifficult = '';
@@ -26,11 +31,26 @@ function selectDifficulty(value) {
     currentDifficult = '';
     currentDifficult = value;
 
+    getCardSet();
     //DEBUG
     console.log(currentDifficult);
+}
+
+function getCardSet() {
+    let greenSum = currentAncient.firstStage.greenCards + currentAncient.secondStage.greenCards + currentAncient.thirdStage.greenCards;
+    let blueSum = currentAncient.firstStage.blueCards + currentAncient.secondStage.blueCards + currentAncient.thirdStage.blueCards;
+    let brownSum = currentAncient.firstStage.brownCards + currentAncient.secondStage.brownCards + currentAncient.thirdStage.brownCards;
+
+
+   //DEBUG
+   console.log(`Зеленые: ${greenSum}`); 
+   console.log(`Синие: ${blueSum}`); 
+   console.log(`Коричнивые: ${brownSum}`);     
 }
 
 showAncient();
 DIFFICLUTIES_INPUTS.forEach(element => {
     element.addEventListener('input', () => selectDifficulty(element.value));
 });
+
+MIX_BUTTON.addEventListener('click', getCardSet);
