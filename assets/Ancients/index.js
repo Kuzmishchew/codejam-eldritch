f7bda121535c46fbffbaddca9cf0e45aca07c3d2
Kuzmishchew <kuzmishchew@gmail.com>
2022-09-05 21:47:08 +0500

    feat: change all links

--- assets/Ancients/index.js
+++ assets/Ancients/index.js
@@ -1,7 +1,7 @@
-let azathoth = '../../assets/Ancients/Azathoth.png';
-let cthulhu = '../../assets/Ancients/Cthulthu.png';
-let iogSothoth = '../../assets/Ancients/IogSothoth.png';
-let shubNiggurath = '../../assets/Ancients/ShubNiggurath.png';
+let azathoth = '/assets/Ancients/Azathoth.png';
+let cthulhu = '/assets/Ancients/Cthulthu.png';
+let iogSothoth = '/assets/Ancients/IogSothoth.png';
+let shubNiggurath = '/assets/Ancients/ShubNiggurath.png';
 
 const ancients = {
   azathoth,
--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
-import ancientsData from '../data/ancients.js';
-import greenCardsData from '../data/mythicCards/green/index.js';
-import brownCardsData from '../data/mythicCards/brown/index.js';
-import blueCardsData from '../data/mythicCards/blue/index.js';
+import ancientsData from '/data/ancients.js';
+import greenCardsData from '/data/mythicCards/green/index.js';
+import brownCardsData from '/data/mythicCards/brown/index.js';
+import blueCardsData from '/data/mythicCards/blue/index.js';
 
 
 const DIFFICLUTY_CONTAINER = document.querySelector('.difficulty-container');
